7c280aa73a7d6288964c581e8369321312904791
xcode <xcode@stdlib.in>
2023-12-19 16:44:59 +0900

    1. update exports

--- func/index.ts
+++ func/index.ts
@@ -3,11 +3,9 @@ import fnFinder from "./finder";
 import migrate from "./migrate";
 import index from "./idx";
 
-const fnMongo = {
+export const fnMongo = {
     ...connect,
     ...fnFinder,
     index,
     migrate,
 };
-
-export default fnMongo;
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export { default as fnMongo } from "./func";
+export { fnMongo } from "./func";
 export { DocKv } from "./doc";
 export * from "./type";
