83dcd007d4ed7d393b3a1b7ecd0eb9be251fa1bc
xcode <xcode@stdlib.in>
2023-12-28 12:11:34 +0900

    1. add MongooseIndex

--- src/type/type.ts
+++ src/type/type.ts
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { IndexDirection, Schema } from "mongoose";
 
 export type Pager = {
     page: number;
@@ -29,5 +29,82 @@ export interface ConnectArg {
     database: string;
 }
 
-export type MongooseModel<T> = import("mongoose").Model<import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>, {}, {}, {}, import("mongoose").IfAny<import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>, any, import("mongoose").Document<unknown, {}, import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>> & import("mongoose").Require_id<import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>>>, Schema<T, import("mongoose").Model<T, any, any, any, import("mongoose").IfAny<T, any, import("mongoose").Document<unknown, any, T> & import("mongoose").Require_id<T>>, any>, {}, {}, {}, {}, import("mongoose").DefaultSchemaOptions, import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>, import("mongoose").IfAny<import("mongoose").FlatRecord<import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>>, any, import("mongoose").Document<unknown, {}, import("mongoose").FlatRecord<import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>>> & import("mongoose").Require_id<import("mongoose").FlatRecord<import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>>>>>>;
+export type MongooseModel<T> = import("mongoose").Model<
+    import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>,
+    {},
+    {},
+    {},
+    import("mongoose").IfAny<
+        import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>,
+        any,
+        import("mongoose").Document<
+            unknown,
+            {},
+            import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>
+        > &
+            import("mongoose").Require_id<
+                import("mongoose").ObtainDocumentType<
+                    any,
+                    T,
+                    import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>
+                >
+            >
+    >,
+    Schema<
+        T,
+        import("mongoose").Model<
+            T,
+            any,
+            any,
+            any,
+            import("mongoose").IfAny<T, any, import("mongoose").Document<unknown, any, T> & import("mongoose").Require_id<T>>,
+            any
+        >,
+        {},
+        {},
+        {},
+        {},
+        import("mongoose").DefaultSchemaOptions,
+        import("mongoose").ObtainDocumentType<any, T, import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>>,
+        import("mongoose").IfAny<
+            import("mongoose").FlatRecord<
+                import("mongoose").ObtainDocumentType<
+                    any,
+                    T,
+                    import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>
+                >
+            >,
+            any,
+            import("mongoose").Document<
+                unknown,
+                {},
+                import("mongoose").FlatRecord<
+                    import("mongoose").ObtainDocumentType<
+                        any,
+                        T,
+                        import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>
+                    >
+                >
+            > &
+                import("mongoose").Require_id<
+                    import("mongoose").FlatRecord<
+                        import("mongoose").ObtainDocumentType<
+                            any,
+                            T,
+                            import("mongoose").ResolveSchemaOptions<import("mongoose").DefaultSchemaOptions>
+                        >
+                    >
+                >
+        >
+    >
+>;
+
 export type FnMigrate<T> = (model: MongooseModel<T>) => Promise<void>;
+
+export interface MongooseIndex {
+    v: number;
+    key: { [key: string]: IndexDirection };
+    name: string;
+    unique: boolean;
+    background: boolean;
+}
